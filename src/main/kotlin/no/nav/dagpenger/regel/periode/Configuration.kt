65d9c9fecddff7042e7848ead7536ba89d3ab8e0
bendreas <andreas.nygard.bergh@nav.no>
2020-04-28 08:03:17 +0200

    Add configuration, dependencies and nais-properties for rapid rivers

--- src/main/kotlin/no/nav/dagpenger/regel/periode/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/periode/Configuration.kt
@@ -13,17 +13,25 @@ import no.nav.dagpenger.streams.KafkaCredential
 import no.nav.dagpenger.streams.Topic
 import no.nav.dagpenger.streams.Topics
 
+private const val TOPIC = "privat-dagpenger-behov-v2"
+
 private val localProperties = ConfigurationMap(
     mapOf(
         "kafka.bootstrap.servers" to "localhost:9092",
+        "kafka.topic" to TOPIC,
+        "kafka.reset.policy" to "earliest",
         "application.profile" to Profile.LOCAL.toString(),
         "application.httpPort" to "8096",
+        "nav.truststore.path" to "",
+        "nav.truststore.password" to "changeme",
         "behov.topic" to Topics.DAGPENGER_BEHOV_PACKET_EVENT.name
     )
 )
 private val devProperties = ConfigurationMap(
     mapOf(
         "kafka.bootstrap.servers" to "b27apvl00045.preprod.local:8443,b27apvl00046.preprod.local:8443,b27apvl00047.preprod.local:8443",
+        "kafka.topic" to TOPIC,
+        "kafka.reset.policy" to "earliest",
         "application.profile" to Profile.DEV.toString(),
         "application.httpPort" to "8096",
         "feature.gjustering" to false.toString(),
@@ -33,6 +41,7 @@ private val devProperties = ConfigurationMap(
 private val prodProperties = ConfigurationMap(
     mapOf(
         "kafka.bootstrap.servers" to "a01apvl00145.adeo.no:8443,a01apvl00146.adeo.no:8443,a01apvl00147.adeo.no:8443,a01apvl00148.adeo.no:8443,a01apvl00149.adeo.no:8443,a01apvl00150.adeo.no:8443",
+        "kafka.topic" to TOPIC,
         "application.profile" to Profile.PROD.toString(),
         "application.httpPort" to "8096",
         "behov.topic" to Topics.DAGPENGER_BEHOV_PACKET_EVENT.name
@@ -58,7 +67,15 @@ data class Configuration(
     data class Kafka(
         val brokers: String = config()[Key("kafka.bootstrap.servers", stringType)],
         val user: String? = config().getOrNull(Key("srvdp.regel.periode.username", stringType)),
-        val password: String? = config().getOrNull(Key("srvdp.regel.periode.password", stringType))
+        val password: String? = config().getOrNull(Key("srvdp.regel.periode.password", stringType)),
+        val rapidApplication: Map<String, String> = mapOf(
+            "KAFKA_BOOTSTRAP_SERVERS" to config()[Key("kafka.bootstrap.servers", stringType)],
+            "KAFKA_CONSUMER_GROUP_ID" to "dp-regel-periode-rapid",
+            "KAFKA_RAPID_TOPIC" to config()[Key("kafka.topic", stringType)],
+            "KAFKA_RESET_POLICY" to config()[Key("kafka.reset.policy", stringType)],
+            "NAV_TRUSTSTORE_PATH" to config()[Key("nav.truststore.path", stringType)],
+            "NAV_TRUSTSTORE_PASSWORD" to config()[Key("nav.truststore.password", stringType)]
+        )
     ) {
         fun credential(): KafkaCredential? {
             return if (user != null && password != null) {
