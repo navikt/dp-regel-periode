3bef069486a06890a51732ff979844729f95ca5b
Atle Nordland <atle.nordland@nav.no>
2021-05-19 09:00:21 +0200

    Oppdatere gjusteringstest

--- src/main/kotlin/no/nav/dagpenger/regel/periode/Fakta.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/periode/Fakta.kt
@@ -53,7 +53,7 @@ data class Fakta(
 internal fun isThisGjusteringTest(
     beregningsdato: LocalDate
 ): Boolean {
-    val gVirkning = LocalDate.of(2021, 4, 12)
+    val gVirkning = LocalDate.of(2021, 5, 24)
     val isBeregningsDatoAfterGjustering = beregningsdato.isAfter(gVirkning.minusDays(1))
     return unleash.isEnabled(GJUSTERING_TEST) && isBeregningsDatoAfterGjustering
 }
