a04b470c25c1fae9f5ff0f56f450521380b8d022
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2023-11-06 12:17:39 +0100

    Deploy til gcp
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- src/main/kotlin/no/nav/dagpenger/regel/periode/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/periode/Configuration.kt
@@ -42,6 +42,7 @@ private val prodProperties =
 private fun config() =
     when (System.getenv("NAIS_CLUSTER_NAME") ?: System.getProperty("NAIS_CLUSTER_NAME")) {
         "dev-fss" -> systemProperties() overriding EnvironmentVariables overriding devProperties
+        "dev-gcp" -> systemProperties() overriding EnvironmentVariables overriding devProperties
         "prod-fss" -> systemProperties() overriding EnvironmentVariables overriding prodProperties
         else -> {
             systemProperties() overriding EnvironmentVariables overriding localProperties
