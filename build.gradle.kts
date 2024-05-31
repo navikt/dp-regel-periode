59204e3ef56fd7aa53ed19332b0fd6730932bd84
André Roaldseth <andre.roaldseth@nav.no>
2024-05-31 10:43:28 +0200

    Ta bort dagpenger-events dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
     implementation(libs.rapids.and.rivers)
 
     implementation("com.github.navikt:dp-inntekt-kontrakter:1_20231220.55a8a9")
-    implementation("com.github.navikt:dagpenger-events:20231220.3050bf")
     implementation("com.github.navikt:dp-grunnbelop:2024.05.30-13.38.6e9169eb05d1")
 
     implementation("de.huxhorn.sulky:de.huxhorn.sulky.ulid:8.3.0")
